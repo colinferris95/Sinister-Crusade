3f3d32437d68d16b083cd560f9cc2a00b27d9b27
Colin <colinferris95@gmail.com>
2013-11-19 12:40:02 -0500

    more functions

--- game.js
+++ game.js
@@ -1,17 +1,15 @@
 
-//acess the help page
+var hidden = true;
+
 function help(){
-	document.getElementById('outputDiv').innerHTML = "Welcome to the Help page! <br> Click the buttons and explore the magic that is sinsister crusade!"
+	document.getElementById('outputDiv').innerHTML = "Hi! This is the help page!"
 	
 }
-//clears the output
+
 function wipe(){
-	document.getElementById('outputDiv').innerHTML = " ";
+	document.getElementById('outputDiv').innerHTML = "";
+	
 }
-
-
-
-var hidden = true;
 
 
 //hides the input for character name creation and confirm and start buttons when html loads(body onload)
@@ -23,7 +21,8 @@ function hide(){
 		document.getElementById("begin").style.display = "none";//Hidden game start button
 		document.getElementById("action").style.display = "none";//hidden action button
 		document.getElementById("walk").style.display = "none";//hidden wak button
-		document.getElementById("wrong").style.display = "none";
+		document.getElementById("wrong").style.display = "none";//hides no turning back
+		document.getElementById("note").style.display = "none";//hides the note button
 	}
 }
 
@@ -31,6 +30,9 @@ function hide(){
 function gameStart(){
 	input.style.display = "";//releaves player input
 	document.getElementById("confirm").style.display = "";//reveals confirm button
+	
+	
+
 }
 
 
@@ -51,20 +53,10 @@ function begin(){
 	output.innerHTML = "";
 	output.innerHTML = input.value + " enters a long hallway";
 	document.getElementById("action").style.display = "";
-<<<<<<< HEAD
-	document.getElementById("picture")
-=======
 	document.getElementById("picture").src="LongHallway.gif";
->>>>>>> Greg-branch
-	
-}
 
-function showStart()
-	{
-    var img = document.getElementById('Sinister-Crusade-Start.gif');
-    img.style.visibility = 'visible';
+
 }
-	
 
 //gives player an option
 function action(){
@@ -76,26 +68,21 @@ function action(){
 
 //allows player to choose
 function walk(){
-	output.innerHTML = input.value + " walks down the hallway";
+	output.innerHTML = input.value + " walks down the hallway and sees a note. Pick up the note?";
 	document.getElementById("walk").style.display = "none";
 	document.getElementById("action").style.display = "none";
 	document.getElementById("wrong").style.display = "none";
-	
+	document.getElementById("note").style.display = "";
 }
 
-//breaks the players soul
 function wrong(){
 	output.innerHTML = "wrong, there is no turning back!!!"
 }
 
+function note(){
+	output.innerHTML = "The path to victory starts with a 2."
+}
 
-
-//player object
-var player;
-
-player.name = document.getElementById("input");
-
->>>>>>> External Changes
 
 function accesPlayer(){
 	var name = player.name;
