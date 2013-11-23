31c801f775f8e2bbb1a6252fdacb50fe261c3013
Colin <colinferris95@gmail.com>
2013-11-22 21:55:28 -0500

    Added new pictures and re-formatted code

--- game.js
+++ game.js
@@ -14,33 +14,48 @@ function wipe(){
 	
 }
 
+
+
 function getStatus(){
-	document.getElementById('outputDiv').innerHTML = "this is the player status";
+	var location = ["start"];
+	var inventory = ["empty"];
+	document.getElementById('outputDiv').innerHTML = "this is the player status " + location[0] + " "  + inventory[0];
+	
 }
 
 
 //hides the input for character name creation and confirm and start buttons when html loads(body onload)
 function hide(){
 	if(hidden === true){
-		var input = document.getElementById("input");//User input
+	    input = document.getElementById("input");//User input
 		input.style.display = "none";
-		document.getElementById("confirm").style.display = "none";//Hidden user confirm button
-		document.getElementById("begin").style.display = "none";//Hidden game start button
-		document.getElementById("action").style.display = "none";//hidden action button
-		document.getElementById("walk").style.display = "none";//hidden wak button
-		document.getElementById("wrong").style.display = "none";//hides no turning back
-		document.getElementById("note").style.display = "none";//hides the note button
-		document.getElementById("exit").style.display = "none";//hides the exit to the staircase
-		document.getElementById("room1").style.display = "none";//room1
-		document.getElementById("room2").style.display = "none";//room2
-		document.getElementById("room3").style.display = "none";//room3
+		confirm = document.getElementById("confirm");
+		confirm.style.display = "none";//Hidden user confirm button
+		begin = document.getElementById("begin");
+		begin.style.display = "none";//Hidden game start button
+		action = document.getElementById("action");
+		action.style.display = "none";//hidden action button
+		walk = document.getElementById("walk");
+		walk.style.display = "none";//hidden wak button
+		wrong = document.getElementById("wrong");
+		wrong.style.display = "none";//hides no turning back
+		note = document.getElementById("note");
+		note.style.display = "none";//hides the note button
+		exit = document.getElementById("exit");
+		exit.style.display = "none";//hides the exit to the staircase
+		room1 = document.getElementById("room1");
+		room1.style.display = "none";//room1
+		room2 = document.getElementById("room2")
+		room2.style.display = "none";//room2
+		room3 = document.getElementById("room3")
+		room3.style.display = "none";//room3
 	}
 }
 
 //revales input and confirm button
 function gameStart(){
 	input.style.display = "";//releaves player input
-	document.getElementById("confirm").style.display = "";//reveals confirm button
+	confirm.style.display = "";//reveals confirm button
 	
 	
 
@@ -48,32 +63,34 @@ function gameStart(){
 
 
 //Shows player inputed name and reveals start button
-function confirm(){
+function final(){
 	
 	output = document.getElementById('outputDiv');
 	output.innerHTML = "Your name is " + input.value;
-	document.getElementById("begin").style.display = "";
+	begin.style.display = "";
 }
 
 //hides the create character button, the output , the input bar and the confirm button, spawns player
-function begin(){
+function start(){
 	input.style.display = "none";
-	document.getElementById("character").style.display = "none";
-	document.getElementById("confirm").style.display = "none";
-	document.getElementById("begin").style.display = "none";
+	character.style.display = "none";
+	confirm.style.display = "none";
+	begin.style.display = "none";
 	output.innerHTML = "";
 	output.innerHTML = input.value + " enters a long hallway";
-	document.getElementById("action").style.display = "";
-	document.getElementById("picture").src="LongHallway.gif";
+	location = "long hallway";
+	action.style.display = "";
+	picture = document.getElementById("picture");
+	picture.src="images/LongHallway.gif";
 
 
 }
 
 //gives player an option
-function action(){
+function hallway(){
 	output.innerHTML = "Would you like to walk down the hallway?";
-	document.getElementById("walk").style.display = "";
-	document.getElementById("wrong").style.display = "";
+	walk.style.display = "";
+	wrong.style.display = "";
 
 }
 
@@ -82,41 +99,42 @@ function wrong(){
 }
 
 //allows player to choose
-function walk(){
+function downTheHallway(){
 	output.innerHTML = input.value + " walks down the hallway and sees a note. Pick up the note?";
-	document.getElementById("walk").style.display = "none";
-	document.getElementById("action").style.display = "none";
-	document.getElementById("wrong").style.display = "none";
-	document.getElementById("note").style.display = "";
-	document.getElementById("exit").style.display = "";
+	walk.style.display = "none";
+	action.style.display = "none";
+	wrong.style.display = "none";
+	note.style.display = "";
+	exit.style.display = "";
 }
 
 //hint for the player
-function note(){
+function hint(){
 	alert("The path to victory starts with a 2.");
-	document.getElementById("note").style.display = "none";
-	document.getElementById("exit").style.display = "";
+	note.style.display = "none";
+	exit.style.display = "";
 }
 
-//leads to the staircase
-function exit(){
-	output.innerHTML = input.value + " encounters a staircase which leads to 3 rooms. Which room do you pick?"
-	document.getElementById("note").style.display = "none";
-	document.getElementById("exit").style.display = "none";
-	document.getElementById("room1").style.display = "";
-	document.getElementById("room2").style.display = "";
-	document.getElementById("room3").style.display = "";
+//leads to the door
+function door(){
+	picture.src = "images/castle-door.gif";
+	output.innerHTML = input.value + " encounters a door which leads to 3 rooms. Which room do you pick?"
+	note.style.display = "none";
+	exit.style.display = "none";
+	room1.style.display = "";
+	room2.style.display = "";
+	room3.style.display = "";
 }
 
-function room1(){
+function roomOne(){
 	
 }
 
-function room2(){
+function roomTwo(){
 	
 }
 
-function room3(){
+function roomThree(){
 	
 }
 
