52197feec1572cc67a28dc3efbd6043209c1f997
Colin <colinferris95@gmail.com>
2013-11-24 17:50:05 -0500

    added inventroy, area, some room function

--- game.js
+++ game.js
@@ -14,12 +14,13 @@ function wipe(){
 	
 }
 
+  bag = "empty";
+  area = "none";
+
 
 
 function getStatus(){
-	var location = ["start"];
-	var inventory = ["empty"];
-	document.getElementById('outputDiv').innerHTML = "this is the player status " + location[0] + " "  + inventory[0];
+	document.getElementById('outputDiv').innerHTML = "Your bag: " + bag + "<br>" + "Your area: " + area;
 	
 }
 
@@ -27,6 +28,7 @@ function getStatus(){
 //hides the input for character name creation and confirm and start buttons when html loads(body onload)
 function hide(){
 	if(hidden === true){
+		picture.src = "images/Sinister-Crusade-Start.gif";
 	    input = document.getElementById("input");//User input
 		input.style.display = "none";
 		confirm = document.getElementById("confirm");
@@ -45,13 +47,57 @@ function hide(){
 		exit.style.display = "none";//hides the exit to the staircase
 		room1 = document.getElementById("room1");
 		room1.style.display = "none";//room1
-		room2 = document.getElementById("room2")
+		room2 = document.getElementById("room2");
 		room2.style.display = "none";//room2
-		room3 = document.getElementById("room3")
+		room3 = document.getElementById("room3");
 		room3.style.display = "none";//room3
+		riddle1 = document.getElementById("riddle1");
+		riddle1.style.display = "none";
+		
+		reset = document.getElementById("reset");
+		reset.style.display = "none";
+		
 	}
 }
 
+function hide2(){
+	if(hidden === true){
+		picture.src = "images/Sinister-Crusade-Start.gif";
+	    input = document.getElementById("input");//User input
+		input.style.display = "none";
+		confirm = document.getElementById("confirm");
+		confirm.style.display = "none";//Hidden user confirm button
+		begin = document.getElementById("begin");
+		begin.style.display = "none";//Hidden game start button
+		action = document.getElementById("action");
+		action.style.display = "none";//hidden action button
+		walk = document.getElementById("walk");
+		walk.style.display = "none";//hidden wak button
+		wrong = document.getElementById("wrong");
+		wrong.style.display = "none";//hides no turning back
+		note = document.getElementById("note");
+		note.style.display = "none";//hides the note button
+		exit = document.getElementById("exit");
+		exit.style.display = "none";//hides the exit to the staircase
+		room1 = document.getElementById("room1");
+		room1.style.display = "none";//room1
+		room2 = document.getElementById("room2");
+		room2.style.display = "none";//room2
+		room3 = document.getElementById("room3");
+		room3.style.display = "none";//room3
+		riddle1 = document.getElementById("riddle1");
+		riddle1.style.display = "none";
+		
+		
+		reset = document.getElementById("reset");
+		reset.style.display = "none";
+		
+		input.style.display = "";//releaves player input
+		confirm.style.display = "";//reveals confirm button
+	
+		document.getElementById('outputDiv').innerHTML = " ";
+	}
+}
 //revales input and confirm button
 function gameStart(){
 	input.style.display = "";//releaves player input
@@ -78,7 +124,7 @@ function start(){
 	begin.style.display = "none";
 	output.innerHTML = "";
 	output.innerHTML = input.value + " enters a long hallway";
-	location = "long hallway";
+	area = "The never-ending hallway";
 	action.style.display = "";
 	picture = document.getElementById("picture");
 	picture.src="images/LongHallway.gif";
@@ -119,6 +165,7 @@ function hint(){
 function door(){
 	picture.src = "images/castle-door.gif";
 	output.innerHTML = input.value + " encounters a door which leads to 3 rooms. Which room do you pick?"
+	area = "door of deciet"
 	note.style.display = "none";
 	exit.style.display = "none";
 	room1.style.display = "";
@@ -127,11 +174,31 @@ function door(){
 }
 
 function roomOne(){
+	// add picture src here greg plz
+	output.innerHTML = "The room is covered in ice. " + input.value + " slips and dies.";
+	room1.style.display = "none";
+	room2.style.display = "none";
+	room3.style.display = "none";
+	reset.style.display = "";
 	
 }
 
 function roomTwo(){
+	//ice axe.jpg
+	output.innerHTML = "answer the riddle to get something sweet!"
+	picture.src="images/RoomWithRiddle1.gif";
+	room1.style.display = "none";
+	room2.style.display = "none";
+	room3.style.display = "none";
+	riddle1.style.display = "";
 	
+}
+
+function riddleOne(){
+	riddle1 = prompt("what has teeth but never eats?", "answer here");
+	if (riddle1 = "comb"){
+		output.innerHTML = "you got the ice axe!";
+	}
 }
 
 function roomThree(){
